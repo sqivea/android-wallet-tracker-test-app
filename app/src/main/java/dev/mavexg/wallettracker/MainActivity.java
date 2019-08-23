e0ef0b981b5530720288d973fe1db9581eb99f9e
Kirill Usachov <usatchoff@gmail.com>
2019-08-23 23:21:05 +0300

    Operations activity added

--- app/src/main/java/dev/mavexg/wallettracker/MainActivity.java
+++ app/src/main/java/dev/mavexg/wallettracker/MainActivity.java
@@ -2,13 +2,41 @@ package dev.mavexg.wallettracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private int mCurrentCashe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setListeners();
+    }
+
+    private void setListeners() {
+        findViewById(R.id.button_main_adding).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, OperationActivity.class);
+                intent.putExtra("operation", "adding");
+                intent.putExtra("current_cash", mCurrentCashe);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.button_main_removing).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, OperationActivity.class);
+                intent.putExtra("operation", "removing");
+                intent.putExtra("current_cash", mCurrentCashe);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/dev/mavexg/wallettracker/OperationActivity.java
+++ app/src/main/java/dev/mavexg/wallettracker/OperationActivity.java
@@ -2,7 +2,12 @@ package dev.mavexg.wallettracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Objects;
 
 public class OperationActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class OperationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_operation);
+
+        Intent currentIntent = getIntent();
+        setupButton(Objects.requireNonNull(currentIntent.getStringExtra("operation")));
+        setListeners();
+    }
+
+    private void setupButton(final String operation) {
+        if (operation.equals("removing")) {
+            Button button = findViewById(R.id.button_operation_direct);
+            button.setBackground(getResources().getDrawable(R.drawable.button_remove_style));
+            button.setText(R.string.text_button_remove_direct);
+        }
+    }
+
+    private void setListeners() {
+        findViewById(R.id.button_operation_direct).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(OperationActivity.this, MainActivity.class));
+            }
+        });
     }
 }
