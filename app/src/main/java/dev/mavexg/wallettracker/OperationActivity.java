75b72b0e573e0b733a43dca5f608699ca289186b
Kirill Usachov <usatchoff@gmail.com>
2019-08-23 21:41:19 +0300

    NumberPicker added

---
+++ app/src/main/java/dev/mavexg/wallettracker/OperationActivity.java
@@ -0,0 +1,14 @@
+package dev.mavexg.wallettracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OperationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_operation);
+    }
+}
